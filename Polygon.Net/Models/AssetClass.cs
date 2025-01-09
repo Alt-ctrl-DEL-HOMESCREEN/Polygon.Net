7d69ef90fdd47c41964ad6da5fcaa55511a1615c
Alt-ctrl-DEL-HOMESCREEN <stevenkingofamerica@gmail.com>
2025-01-09 13:49:03 -0500

    Delete Polygon.Net directory

--- Polygon.Net/Models/AssetClass.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Polygon.Net.Models;
-
-public static class AssetClass
-{
-    public static readonly string Stocks = "stocks";
-    public static readonly string Options = "options";
-    public static readonly string Crypto = "crypto";
-    public static readonly string Fx = "fx";    
-}
--- Polygon.Net/Models/Locale.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Polygon.Net.Models;
-
-public static class Locale
-{
-    public static readonly string Us = "us";
-    public static readonly string Global = "global";
-}
